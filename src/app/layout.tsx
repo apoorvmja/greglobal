d54cf25008899a81bc1de4a4cef3d4d7ce035fb5
apoorv <apoorv@mja.in>
2024-08-21 19:39:05 +0530

    small css changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,10 +10,10 @@ export const metadata: Metadata = {
     process.env.APP_URL
       ? `${process.env.APP_URL}`
       : process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
-      : `http://localhost:${process.env.PORT || 3000}`
+        ? `https://${process.env.VERCEL_URL}`
+        : `http://localhost:${process.env.PORT || 3000}`
   ),
-  title: "shadcn/ui sidebar",
+  title: "GreGoGlobal -Practice Gre With full length mock tests",
   description:
     "A stunning and functional retractable sidebar for Next.js built on top of shadcn/ui complete with desktop and mobile responsiveness.",
   alternates: {
@@ -21,14 +21,14 @@ export const metadata: Metadata = {
   },
   openGraph: {
     url: "/",
-    title: "shadcn/ui sidebar",
+    title: "GreGoGlobal -Practice Gre With full length mock tests",
     description:
       "A stunning and functional retractable sidebar for Next.js built on top of shadcn/ui complete with desktop and mobile responsiveness.",
     type: "website"
   },
   twitter: {
     card: "summary_large_image",
-    title: "shadcn/ui sidebar",
+    title: "GreGoGlobal -Practice Gre With full length mock tests",
     description:
       "A stunning and functional retractable sidebar for Next.js built on top of shadcn/ui complete with desktop and mobile responsiveness."
   }
--- src/components/mock-tests-ui/TotalTestDisplay.tsx
+++ src/components/mock-tests-ui/TotalTestDisplay.tsx
@@ -21,11 +21,9 @@ const TotalTestDisplay: React.FC = () => {
         <section className="dark:bg-gray-900 dark:text-white rounded-lg">
             <div className="max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
                 <div className="max-w-xl">
-                    <h2 className="text-3xl font-bold sm:text-4xl">What makes us special</h2>
+                    <h2 className="text-3xl font-bold sm:text-4xl">Full length practice tests</h2>
                     <p className="mt-4 dark:text-gray-300">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat dolores iure fugit totam
-                        iste obcaecati. Consequatur ipsa quod ipsum sequi culpa delectus, cumque id tenetur
-                        quibusdam, quos fuga minima.
+                        Welcome to our comprehensive test preparation platform, where you can explore and navigate through a variety of practice tests designed to sharpen your skills and boost your confidence.
                     </p>
                 </div>
 
