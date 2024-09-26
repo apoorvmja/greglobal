b071c9b436ed514224536eae7a8cf0e972faee2b
apoorv <apoorv@mja.in>
2024-09-26 10:15:46 +0530

    bug fixes for test 4

--- src/app/(demo)/mock-tests/[[...rest]]/page.tsx
+++ src/app/(demo)/mock-tests/[[...rest]]/page.tsx
@@ -11,6 +11,37 @@ import {
   BreadcrumbSeparator
 } from "@/components/ui/breadcrumb";
 import TotalTestDisplay from "@/components/mock-tests-ui/TotalTestDisplay";
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Free Shorter GRE Mock Tests - Take Free Test Today',
+  description: 'AI-powered GRE full-length mock tests available online. Make your GRE prepration jouney smooth as never before, launching First even AI powered GRE Mock tests in the market.',
+  keywords: ['GRE mock test', 'Free GRE mock tests', 'Shorter GRE mock tests', 'free shorter gre mock tests', 'gre practice tests', 'gregoglobal practice tests'],
+  openGraph: {
+    title: 'Free Shorter GRE Mock Tests - Take Free Test Today',
+    description: 'AI-powered GRE full-length mock tests available online. Make your GRE prepration jouney smooth as never before, launching First even AI powered GRE Mock tests in the market.',
+    url: 'https://gregoglobal.com/mock-tests',
+    images: [
+      {
+        url: 'https://gregoglobal.com/GreGoGlobalLandingPage.webp',
+        alt: 'Free Shorter GRE Mock tests',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Free Shorter GRE Mock Tests - Take Free Test Today',
+    description: 'AI-powered GRE full-length mock tests available online. Make your GRE prepration jouney smooth as never before, launching First even AI powered GRE Mock tests in the market.',
+    images: ['https://gregoglobal.com/GreGoGlobalLandingPage.webp'],
+  },
+  alternates: {
+    canonical: 'https://gregoglobal.com/mock-tests',
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+};
 
 export default function CategoriesPage() {
   return (
