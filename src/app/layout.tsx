d76f6d33e79eb8df9f4111170cd315792ad8d3d2
apoorv <apoorv@mja.in>
2024-12-19 14:52:58 +0530

    gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ import {
 } from '@clerk/nextjs'
 
 import './globals.css'
+import Script from "next/script";
 
 export const metadata: Metadata = {
   metadataBase: new URL(
@@ -44,9 +45,24 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const GTM_ID = "AW-16486749894"
+
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id=${GTM_ID}';f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `,
+          }}
+        />
         <body className={GeistSans.className}>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             {children}
